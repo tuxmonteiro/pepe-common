fa93cffc9e0d8652f145f2da9763b7fb1e30d828
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2019-05-03 16:57:58 -0300

    Split Constants.TRIGGER_PREFIX into PACK_NAME/new TRIGGER_NAME

--- src/main/java/com/globo/pepe/common/util/Constants.java
+++ src/main/java/com/globo/pepe/common/util/Constants.java
@@ -2,6 +2,7 @@ package com.globo.pepe.common.util;
 
 public interface Constants {
 
-    String TRIGGER_PREFIX = "pepe.trigger";
+    String PACK_NAME = "pepe";
+    String TRIGGER_PREFIX = "trigger";
 
 }
