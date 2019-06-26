17c0dd44ceb0dd788c0596d78072d1cc2d939d53
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2019-06-26 01:43:16 -0300

    Remove unnecessary imports

--- src/main/java/com/globo/pepe/common/Application.java
+++ src/main/java/com/globo/pepe/common/Application.java
@@ -18,8 +18,6 @@ package com.globo.pepe.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class Application {
