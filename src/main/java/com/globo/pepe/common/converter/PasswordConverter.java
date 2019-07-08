625fa50f16aeded0b48991c5643eb3b27d0d6e11
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2019-07-08 04:37:07 -0300

    Improve password security with new aproach using salt and CBC

--- src/main/java/com/globo/pepe/common/converter/PasswordConverter.java
+++ src/main/java/com/globo/pepe/common/converter/PasswordConverter.java
@@ -17,26 +17,54 @@
 package com.globo.pepe.common.converter;
 
 import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
-import java.security.Key;
-import java.util.Base64;
+import javax.xml.bind.DatatypeConverter;
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.AlgorithmParameters;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Optional;
 
 @Converter
 public class PasswordConverter implements AttributeConverter<String, String> {
 
-    private static final String ALGORITHM = "AES/ECB/PKCS5Padding";
-    private static final byte[] KEY = Optional.ofNullable(System.getenv("PEPE_SECRET")).orElse("mysecret").getBytes();
+    private static final char[] KEY = Optional.ofNullable(System.getenv("PEPE_SECRET")).orElse("mysecret").toCharArray();
 
     @Override
     public String convertToDatabaseColumn(String password) {
-        Key key = new SecretKeySpec(KEY, "AES");
         try {
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            return Base64.getEncoder().encodeToString(cipher.doFinal(password.getBytes()));
+            SecureRandom random = new SecureRandom();
+            byte[] salt = new byte[16];
+            random.nextBytes(salt);
+            SecretKey secretKey = getSecretKey(salt);
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            final byte[] passwordBytes = password.getBytes(StandardCharsets.UTF_8.name());
+            byte[] encryptedText = getBytes(null, passwordBytes, secretKey, Cipher.ENCRYPT_MODE, cipher);
+
+            AlgorithmParameters params = cipher.getParameters();
+            byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+
+            // concatenate salt + iv + ciphertext
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            outputStream.write(salt);
+            outputStream.write(iv);
+            outputStream.write(Objects.requireNonNull(encryptedText));
+
+            // properly encode the complete ciphertext
+            return DatatypeConverter.printBase64Binary(outputStream.toByteArray());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -44,13 +72,42 @@ public class PasswordConverter implements AttributeConverter<String, String> {
 
     @Override
     public String convertToEntityAttribute(String dbData) {
-        Key key = new SecretKeySpec(KEY, "AES");
         try {
-            Cipher c = Cipher.getInstance(ALGORITHM);
-            c.init(Cipher.DECRYPT_MODE, key);
-            return new String(c.doFinal(Base64.getDecoder().decode(dbData)));
+            byte[] ciphertext = DatatypeConverter.parseBase64Binary(dbData);
+            if (ciphertext.length < 48) {
+                return null;
+            }
+            byte[] salt = Arrays.copyOfRange(ciphertext, 0, 16);
+            byte[] iv = Arrays.copyOfRange(ciphertext, 16, 32);
+            byte[] ct = Arrays.copyOfRange(ciphertext, 32, ciphertext.length);
+
+            SecretKey secretKey = getSecretKey(salt);
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            byte[] plaintext = getBytes(iv, ct, secretKey, Cipher.DECRYPT_MODE, cipher);
+
+            return new String(Objects.requireNonNull(plaintext), StandardCharsets.UTF_8.name());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private byte[] getBytes(byte[] iv, byte[] ct, SecretKey secretKey, int mode, Cipher cipher) throws GeneralSecurityException {
+        SecretKey secret = new SecretKeySpec(secretKey.getEncoded(), "AES");
+        if (Cipher.DECRYPT_MODE == mode) {
+            cipher.init(mode, secret, new IvParameterSpec(iv));
+            return cipher.doFinal(ct);
+        }
+        if (Cipher.ENCRYPT_MODE == mode) {
+            cipher.init(mode, secret);
+            return cipher.doFinal(ct);
+        }
+        return null;
+    }
+
+    private SecretKey getSecretKey(byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeySpec spec = new PBEKeySpec(KEY, salt, 65536, 256);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        return factory.generateSecret(spec);
     }
 }
